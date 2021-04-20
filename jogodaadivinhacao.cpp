8ead1ae821bcdd66d349999f84c237ae886249ca
Leandro <lealmeids@gmail.com>
2021-04-20 18:02:26 -0400

    Primeiro programa

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; //indica que as funções serão usadas com frequência
+
+int main (){
+    cout << "*************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhacao *" << endl;
+    cout << "*************************************" << endl;
+}
+
+//Para compilar no cmd, estando na pasta do arquivo:
+//g++ jogodaadivinhacao.cpp -o jogodaadivinhacao.out
+//Para build:
+//./jogodaadivinhacao.out
