285173c7eb993e8ad46c848158621ee41514a1ab
Leandro <lealmeids@gmail.com>
2021-04-21 18:39:55 -0400

    Chutando números

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -7,7 +7,18 @@ int main (){
     cout << "*************************************" << endl;
 
     int numero_secreto = 42;
-    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" <<endl;
+    int chute;
+    cout << "Qual seu chute? ";
+    cin >> chute;
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    if (chute == numero_secreto){
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    }else if (chute > numero_secreto){
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }else {
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
 }
 
 //Para compilar no cmd, estando na pasta do arquivo:
