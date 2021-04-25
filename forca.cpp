14c3f33ac74bf00432a0b0eb5c0fd1347e353887
Leandro <lealmeids@gmail.com>
2021-04-25 19:22:48 -0400

    Escrevendo a palavra secreta

--- forca.cpp
+++ forca.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std; //indica que as funções serão usadas com frequência
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute)
 {
@@ -13,21 +15,33 @@ bool letra_existe(char chute)
             return true;
         }
     }
-    return false;  
+    return false;
 }
 
 int main()
 {
-    cout << PALAVRA_SECRETA << endl;
-
     //Variáveis
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while (nao_acertou && nao_enforcou)
     {
+        for (char letra : PALAVRA_SECRETA)
+        {
+            if (chutou[letra]) //Mapeia a ocorrência de uma letra na palavra
+            {
+                cout << letra << " ";
+            } else
+            {
+                cout << "_ ";
+            }
+            
+        }
+        cout << endl << "Escolha uma letra para chutar: " << endl;
         char chute;
         cin >> chute;
+        chutou [chute] = true;
+
         cout << "O seu chute foi: " << chute << endl;
 
         if (letra_existe(chute))
