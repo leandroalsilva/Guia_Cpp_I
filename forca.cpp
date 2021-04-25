e98f047a6825ffc3b60036a2b727073805afdab7
Leandro <lealmeids@gmail.com>
2021-04-24 22:16:03 -0400

    Usando strings

---
+++ forca.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std; //indica que as funções serão usadas com frequência
+
+int main()
+{
+    string palavra_secreta = "MELANCIA";
+    cout << palavra_secreta <<endl;
+}
