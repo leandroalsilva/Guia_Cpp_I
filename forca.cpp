8de00a494ce0fd2f0382ff470fa7b8f63872d520
Leandro <lealmeids@gmail.com>
2021-04-25 18:28:35 -0400

    Verificando chutes

--- forca.cpp
+++ forca.cpp
@@ -2,10 +2,23 @@
 #include <string>
 using namespace std; //indica que as funções serão usadas com frequência
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char c)
+{
+    for (int i = 0; i < PALAVRA_SECRETA.size(); i++)
+    {
+        if (c == PALAVRA_SECRETA[i])
+        {
+            return true;
+        }
+    }
+    return false;  
+}
+
 int main()
 {
-    string palavra_secreta = "MELANCIA";
-    cout << palavra_secreta <<endl;
+    cout << PALAVRA_SECRETA << endl;
 
     //Variáveis
     bool nao_acertou = true;
@@ -15,7 +28,15 @@ int main()
     {
         char chute;
         cin >> chute;
-        cout << "O seu chute foi: " << chute <<endl;
+        cout << "O seu chute foi: " << chute << endl;
+
+        if (letra_existe(chute))
+        {
+            cout << "Você acertou! Seu chute está na palavra" << endl;
+        }
+        else
+        {
+            cout << "Você errou! Seu chute não está na palavra" << endl;
+        }
     }
-    
 }
