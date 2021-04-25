895ba9c8732c460f4d6d56a50630cfa4e563331b
Leandro <lealmeids@gmail.com>
2021-04-24 21:01:33 -0400

    Adicionando níveis de dificuldade

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -6,31 +6,50 @@ int main()
     cout << "*************************************" << endl;
     cout << "* Bem-vindos ao jogo da adivinhacao *" << endl;
     cout << "*************************************" << endl;
+    cout << "Escolha o seu nível de dificuldade" << endl;
+    cout << "Fácil (F), Médio (M) ou Difícil (D)" << endl;
 
     //Variáveis
     const int NUMERO_SECRETO = 42;
     bool nao_acertou = true;
-    int tentativas = 0;
+    int tentativas;
     double pontos = 1000;
+    char dificuldade;
+    int numero_tentativas;
 
-    while (nao_acertou)
+    cin >> dificuldade;
+
+    if (dificuldade == 'F')
+    {
+        numero_tentativas = 15;
+    }
+    else if (dificuldade == 'M')
+    {
+        numero_tentativas = 10;
+    }
+    else
+    {
+        numero_tentativas = 5;
+    }
+
+    for (tentativas = 1; tentativas <= numero_tentativas; tentativas++)
     {
         int chute;
-        tentativas++;
         cout << "Tentativa " << tentativas << endl;
         cout << "Qual seu chute? ";
         cin >> chute;
         bool acertou = chute == NUMERO_SECRETO;
         bool maior = chute > NUMERO_SECRETO;
 
-        double pontos_perdidos = abs((long)((chute - NUMERO_SECRETO)/2.0)); 
+        double pontos_perdidos = abs((long)((chute - NUMERO_SECRETO) / 2.0));
 
         cout << "O valor do seu chute é: " << chute << endl;
-            
+
         if (acertou)
         {
             cout << "Parabéns! Você acertou o número secreto!" << endl;
             nao_acertou = false;
+            break;
         }
         else if (maior)
         {
@@ -43,10 +62,17 @@ int main()
         pontos = pontos - pontos_perdidos;
     }
     cout << "Fim de jogo!" << endl;
-    cout << "Você acertou o número secreto em " << tentativas << " tentativas" << endl;
-    cout.precision(2); //precisão das casas decimais
-    cout << fixed; //modificador fixo da vírgula
-    cout << "Sua pontuação é " << pontos << endl;
+    if (nao_acertou)
+    {
+        cout << "Você perdeu! Tente novamente!" << endl;
+    }else 
+    {
+        cout << "Você acertou o número secreto em " << tentativas << " tentativas" << endl;
+        cout.precision(2); //precisão das casas decimais
+        cout << fixed;     //modificador fixo da vírgula
+        cout << "Sua pontuação é " << pontos << endl; 
+    }
+
 }
 
 //Para compilar no cmd, estando na pasta do arquivo:
