cc8de52542ce504c7a49672be51d3fcf278c734c
Leandro <lealmeids@gmail.com>
2021-04-25 21:28:36 -0400

    Problemas com arquivos

--- forca.cpp
+++ forca.cpp
@@ -99,19 +99,28 @@ vector<string> le_arquivo()
 {
     ifstream arquivo;
     arquivo.open("palavras.txt");
-    
-    int quantidade_palavras;
-    arquivo >> quantidade_palavras;
 
-    vector<string> palavras_arquivo;
-
-    for (int i = 0; i < quantidade_palavras; i++)
+    if (arquivo.is_open())
     {
-        string palavra_lida;
-        arquivo >> palavra_lida;
-        palavras_arquivo.push_back(palavra_lida);
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
+
+        vector<string> palavras_arquivo;
+
+        for (int i = 0; i < quantidade_palavras; i++)
+        {
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            palavras_arquivo.push_back(palavra_lida);
+        }
+        arquivo.close();
+        return palavras_arquivo;
     }
-    return palavras_arquivo;
+    else
+    {
+        cout << "Não foi possível acessar o banco de palavras" << endl;
+        exit(0);
+    }
 }
 
 void sorteia_palavra()
